b95b4e96cb3f220358c6c51a44fcf2b157b38fc7
Abdulla <shameel.abdulla@qatar.tamu.edu>
2025-08-25 13:35:24 +0300

    Updated assignment 1 for fall 2025

--- Programming in Python/auto_grading_script.py
+++
@@ -1,6 +0,0 @@
-from my_tools import lettergrade
-import pandas as pd
-data = pd.read_csv('scores.csv')
-result = data
-result['grade'] = data['score'].apply(lettergrade)
-result.to_csv('grades.csv', index=False)
